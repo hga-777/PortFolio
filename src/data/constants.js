d61690cea65ad862b6068ef14e524f08344d34f5
Hritik Anand <realhga@gmail.com:--global user.email>
2024-08-29 20:05:40 +0530

    Completion 1

---
+++ src/data/constants.js
@@ -0,0 +1,304 @@
+import Blobby from "../images/Blobby.png"
+import LetsMeet from "../images/LetsMeet.png"
+import MAuth from "../images/MAuth.png"
+import SumIT from "../images/SumIT.png"
+import Tchat from "../images/TChat.png"
+
+export const Bio = {
+    name: "Hritik Anand",
+    roles: [
+      "Full Stack Developer",
+      "Competitive Programmer",
+      "Youtuber",
+      "Educator",
+    ],
+    description:
+      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/hga-777",
+    resume:
+      "https://drive.google.com/file/d/19vXHlUbH4_xc44p2MZnYLlXPZu21uvvj/view?usp=drive_link",
+    linkedin: "https://www.linkedin.com/in/hritik-anand-b60a3a224/",
+    youtube: "https://www.youtube.com/@gad-an-tik",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "Redux",
+          image:
+            "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+        },
+        {
+          name: "Next Js",
+          image:
+            "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "TypeScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/768px-Typescript_logo_2020.svg.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+        {
+          name: "Shandcn/ui",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTFP5nxXehZrk7fB5W_TaQ2TilhZdzNH8rSw&s",
+        },
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "https://nodejs.org/static/images/logo.svg",
+        },
+        {
+          name: "Express Js",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXpCVrym3sQKRS3_hAnLQIocLuMJorvv0Jiw&s",
+        },
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+        {
+          name: "Postgresql",
+          image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+        },
+        {
+          name: "MongoDB",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        },
+        {
+          name: "Firebase",
+          image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+        },
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Git",
+          image:
+            "https://www.freecodecamp.org/news/content/images/2022/09/git-branches.png",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "Netlify",
+          image:
+            "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "Postman",
+          image:
+            "https://yt3.googleusercontent.com/X-rhKMndFm9hT9wIaJns1StBfGbFdLTkAROwm4UZ3n9ucrBky5CFIeeZhSszFXBgQjItzCD0SA=s900-c-k-c0x00ffffff-no-rj",
+        },
+        {
+          name: "Insomnia",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJiSvJyPl2_jzBMakkNPavVEOS1xzEs-j2tg&s",
+        },
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrfeJJP-Q-Yuk9IxPlX9hsYM999m9BdTQnhQ&s",
+      role: "Web Development Intern",
+      company: "Navodita Infotech",
+      date: "May 2024 - June 2024",
+      desc: "Created blog website under the guidance of Nivodita Infotech",
+      skills: [
+        "MongoDB",
+        "Tailwind CSS",
+        "Express JS",
+        "JavaScript",
+        "TypeScript",
+        "Node Js",
+        " Next Js",
+      ],
+      doc: "https://drive.google.com/file/d/1tk4FU6Fu-bbbZbai0qrNlKd7V5_Grq6g/view?usp=sharing",
+    },
+    {
+      id: 1,
+      img: "https://media.licdn.com/dms/image/D4E0BAQH-lioYmP95dA/company-logo_200_200/0/1713896326318?e=2147483647&v=beta&t=gAzgsQ2jhSqyqztTThPMVyvvZ6C90zC6YFNSukorEdU",
+      role: "Web Development Intern",
+      company: "Encryptix",
+      date: "May 2024 - June 2024",
+      desc: "Built multiple mini projects like Calculator, Landing pages, Tribute Pages for the sake of learning",
+      skills: [
+        "ReactJS",
+        "Redux",
+        "NodeJs",
+        "Material UI",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "MongoDB",
+      ],
+      doc: "https://drive.google.com/file/d/1D2KIWWQij2GoqAJbso6WuzuBslCcIHdS/view",
+    },
+    {
+      id: 2,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRU6I89qWTpIKrBbP7kYD74jLzMw20h_DeOxw&s",
+      role: "Content Creator",
+      company: "Gad_An_Tik",
+      date: "May 2022 - Present",
+      desc: "Contributing to the field and competitve programming by sharing my approach of solving problems during contest",
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://people.iitism.ac.in/~MAS-2024/images/iitsim.png",
+      school: "Indian Institute of Technology (Indian School of Mines), Dhanbad",
+      date: "Dec 2021 - May 2025",
+      grade: "8.25 CGPA",
+      desc: "I am currently pursuing a Bachelor's degree in Electronics and Communication Engineering at Indian Institute of Technology (Indian School of Mines), Dhanbad. I have completed 6 semesters and have a CGPA of 8.25. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+      degree: "Bachelor of Technology - BTech, Electronics and Communication Engineering",
+    },
+    {
+      id: 1,
+      img: "https://scontent.fccu17-1.fna.fbcdn.net/v/t39.30808-6/346264021_978399266915218_8668251771977751486_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=k6MIExOG91MQ7kNvgEp1NM_&_nc_ht=scontent.fccu17-1.fna&oh=00_AYCpTUzPXyLhgq67AHmPXr0_F4196VOsH5NnwE7TtskSSg&oe=66B3B144",
+      school: "Satyam International School, Patna, Bihar",
+      date: "Apr 2019 - May 2021",
+      grade: "93.2%",
+      desc: "I completed my class 12 high school education at Satyam International School, Patna, Bihar, where I studied Physics, Chemistry and Maths.",
+      degree: "CBSE(XII), Science(PCM)",
+    },
+    {
+      id: 2,
+      img: "https://www.shivamconvent.com/images/logo.png",
+      school: "Shivam Convent, Patna, Bihar",
+      date: "Apr 2006 - Apr 2019",
+      grade: "97.2%",
+      desc: "I completed my class 10 education at Shivam Convent, Patna, Bihar, where I studied Science with Computer Application.",
+      degree: "CBSE(X)",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 9,
+      title: "Lets Meet",
+      date: "Jun 2024 - Present",
+      description:
+        "I implemented robust authentication using Clerk, supporting multiple social sign-ins, and developed comprehensive meeting features such as recording, emoji reactions, screen sharing, layout customization, and participant management. Additionally, I enabled meeting scheduling and history management, including options to view and manage recorded meetings. I also created personal meeting rooms with unique links and user-specific details, and integrated the Stream API for secure, real-time video/audio conferencing with a responsive design.",
+      image: LetsMeet,
+      tags: [
+        "Stream",
+        "Clerk",
+        "Next Js",
+        "MongoDb",
+        "Node Js",
+        "Express Js",
+        "Redux",
+      ],
+      category: "web app",
+      github: "https://github.com/hga-777/LetS-Meet",
+    },
+    {
+      id: 0,
+      title: "Blobby",
+      date: "Feb 2024 - Present",
+      description:
+        "I developed a full-stack blog application using the MERN stack, featuring robust JWT-based authentication and Google OAuth integration. I implemented Redux Toolkit for efficient state management and ensured secure access to admin-specific pages with role-based access control. The admin dashboard includes management of posts, comments, and users. Additionally, I integrated advanced search functionality, allowing users to search, sort, and filter posts efficiently with a modern sidebar and optimized search queries. The responsive, user-friendly UI was built using Tailwind CSS and Flowbite, including a dark mode for an enhanced user experience.",
+      image: Blobby,
+      tags: ["React", "Node.js", "Express.js", "MongoDB", "Firebase","Oauth", "Tailwind CSS", "Flowbite", "HTML"],
+      category: "web app",
+      github: "https://github.com/hga-777/Bloggy",
+      webapp: "https://blobby-lvx2.onrender.com/",
+    },
+    {
+      id: 1,
+      title: "True Chat",
+      date: "Oct 2023 - Present",
+      description:
+        "I developed a real-time chat website with user registration and authentication, using React for the front end and Node.js with MongoDB for the back end. The project features user-friendly registration and login functionality, integrated Socket.io for real-time messaging, and a dynamic avatar selection system that allows users to personalize their avatars.",
+      image: Tchat,
+      tags: [
+        "React Js",
+        "MongoDb",
+        "Node Js",
+        "Express Js",
+        "Redux",
+        "Socket io",
+      ],
+      category: "web app",
+      github: "https://github.com/hga-777/TrueChat",
+      webapp: "https://truechat.onrender.com/login",
+    },
+    {
+      id: 2,
+      title: "Sum IT",
+      date: "Oct 2023 - Present",
+      description:
+        "I created a user-friendly interface that allows users to enter a URL containing a large article, convert it into speech, and listen to a real-time audio summary. The system supports different voices and notes for the audio, using Rapid API's Article Extractor and Summarizer to generate concise summaries. Additionally, I implemented responsive web design to ensure accessibility and usability across various devices and screen sizes.",
+      image: SumIT,
+      tags: ["React", "HTML", "CSS", "Rapid API"],
+      category: "web app",
+      github: "https://github.com/hga-777/SumIT",
+      webapp: "https://splendid-tarsier-788bb9.netlify.app/",
+    },
+    {
+      id: 3,
+      title: "MAuth",
+      date: "Dec 2023 - Present",
+      description:
+        "I developed a robust authentication system using MongoDB, React, Node.js, and Express, integrating Firebase OAuth for scalable user authentication. The system includes comprehensive user management features such as account creation, updates, and deletions. I designed an intuitive React frontend with Redux Toolkit for efficient state management and ensured data security through encrypted communication, JWT implementation, and Firebase OAuth.",
+      image: MAuth,
+      tags: ["React", "Node.js", "Express.js", "MongoDB", "Firebase OAuth"],
+      category: "web app",
+      github: "https://github.com/hga-777/MAuth",
+      webapp: "https://mauth-92xv.onrender.com/",
+    },  
+  ];
+  
+  export const TimeLineData = [
+    { year: 2017, text: "Started my journey" },
+    { year: 2018, text: "Worked as a freelance developer" },
+    { year: 2019, text: "Founded JavaScript Mastery" },
+    { year: 2020, text: "Shared my projects with the world" },
+    { year: 2021, text: "Started my own platform" },
+  ];
